b2119550cde3c20d3e5a0f30c5d485bf49e8796f
Thimanjila <tdudangawe@gmail.com>
2026-07-07 16:01:06 +0530

    Add game screen layout

--- TapFrenzy/ContentView.swift
+++ TapFrenzy/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 24) {
+            Text("Tap Frenzy").font(.largeTitle.bold())
+            Text("Score: 0").font(.title.bold())
+            Button("TAP") {}
+                .frame(width: 150, height: 150)
+                .background(Color.green)
+                .clipShape(Circle())
         }
-        .padding()
     }
 }
 
