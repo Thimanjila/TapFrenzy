c76b1efc377e972722a14a81fb1d6a03b9f66660
Thimanjila <tdudangawe@gmail.com>
2026-07-07 23:19:48 +0530

    Add Card model

---
+++ TapFrenzy/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  TapFrenzy
+//
+//  Created by Thimanjila Udangawe on 2026-07-07.
+//
+
+import Foundation
+
+struct Card: Identifiable {
+    let id: Int
+    var isLit: Bool = false
+}
